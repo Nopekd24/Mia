ab493abeebad061576541ebdaec429de0b7ce36e
Tech Shreyansh <165946497+TechyShreyansh@users.noreply.github.com>
2024-10-10 21:33:54 +0530

    Update config.py
    Signed-off-by: Tech Shreyansh <165946497+TechyShreyansh@users.noreply.github.com>

--- FallenRobot/config.py
+++ FallenRobot/config.py
@@ -2,7 +2,7 @@ class Config(object):
     LOGGER = True
 
     # Get this value from my.telegram.org/apps
-    API_ID = 6
+    API_ID = 214586378
     API_HASH = "eb06d4abfb49dc3eeb1aeb98ae0f581e"
 
     CASH_API_KEY = ""  # Get this value for currency converter from https://www.alphavantage.co/support/#api-key
@@ -16,13 +16,13 @@ class Config(object):
     # Telegraph link of the image which will be shown at start command.
     START_IMG = "https://github.com/TechyShreyansh/Mia/blob/master/FallenRobot/resources/Mia.png"
 
-    SUPPORT_CHAT = "techy_shreyansh2"  # Your Telegram support group chat username where your users will go and bother you
+    SUPPORT_CHAT = "Techy_Shreyansh2"  # Your Telegram support group chat username where your users will go and bother you
 
     TOKEN = ""  # Get bot token from @BotFather on Telegram
 
     TIME_API_KEY = ""  # Get this value from https://timezonedb.com/api
 
-    OWNER_ID = 1356469075  # User id of your telegram account (Must be integer)
+    OWNER_ID = 6910445402  # User id of your telegram account (Must be integer)
 
     # Optional fields
     BL_CHATS = []  # List of groups that you want blacklisted.
